cbeb021b066ca6498c9a85c49023c75f8dce9164
Andy Dickinson <dickinson.andrew@hotmail.co.uk>
2023-02-09 09:42:47 +0000

    practice code added, notes updated

---
+++ practice code/loop.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int x = 100000;
+    while (x>0){
+        printf("%d\n", x);
+        x = x - 1;
+	}
+	return 0;
+}
---
+++ practice code/read-write.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    // declare file pointer and open file in read/write mode
+    FILE *fptr;
+    fptr = fopen("test-file-c.txt", "w+");
+
+    // write to file
+    fprintf(fptr, "%s %s %d", "hello", "world!", 2023);
+    fprintf(fptr, "\nyo");
+
+    // sets file position to the beginning of the file
+    rewind(fptr);
+
+    // create string to store read text
+    char my_text[100];
+
+    // if file exits
+    if (fptr != NULL)
+    {
+        // while something to read, read and print
+        while (fgets(my_text, 100, fptr))
+        {
+            printf("%s", my_text);
+        }
+    } else {
+        printf("Not able to open the file.");
+    }
+
+    // close file
+    fclose(fptr);
+    return 0;
+}
